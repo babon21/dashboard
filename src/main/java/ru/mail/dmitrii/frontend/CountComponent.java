debaa0ee123d0adc98c0ed9fb856f6b2ffebfab4
Darzhain Dmitrii <dimad_1996@mail.ru>
2019-03-08 01:46:15 +0700

    added count layout, count component complete

--- src/main/java/ru/mail/dmitrii/frontend/CountComponent.java
+++ src/main/java/ru/mail/dmitrii/frontend/CountComponent.java
@@ -3,6 +3,8 @@ package ru.mail.dmitrii.frontend;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import ru.mail.dmitrii.service.CountService;
 
@@ -11,11 +13,19 @@ public class CountComponent extends Composite<Div> {
     public CountComponent() {
         getContent().setClassName("my-count");
 
+
+
         Label title = new Label("Счетчик посещений");
         title.setClassName("count-title");
         Label countLabel = new Label(  "" + CountService.getCount());
-        VerticalLayout verticalLayout = new VerticalLayout(title, countLabel);
+        VerticalLayout countLayout = new VerticalLayout(countLabel);
+        countLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        countLayout.setClassName("count-layout");
+        countLayout.setWidth("60%");
+
+        VerticalLayout verticalLayout = new VerticalLayout(title, countLayout);
         verticalLayout.setSizeFull();
+        verticalLayout.setAlignItems(FlexComponent.Alignment.CENTER);
         getContent().add(verticalLayout);
     }
 }
